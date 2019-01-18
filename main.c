f9c1ea3cf99a229383e125690bca46aeb010d510
小智 <123123@qq.com>
2019-01-18 10:49:19 +0800

    添加学生成绩

--- main.c
+++ main.c
@@ -31,6 +31,22 @@ int main(){
 
         
         if (code == 1) {
+            printf("请输入学生姓名：\n");
+            scanf("%s",arr[index].name);
+
+            printf("请输入学生语文成绩：\n");
+            scanf("%d",&(arr[index].yuwen));
+            printf("请输入学生数学成绩：\n");
+            scanf("%d",&(arr[index].shuxue));
+            printf("请输入学生英语成绩：\n");
+            scanf("%d",&(arr[index].yingyu));
+
+            index++;
+
+            printf("添加成功，点击回车继续\n");
+            char x ;
+            scanf("%c",&x);
+            scanf("%c",&x);
             
         }
         if (code == 2) {
